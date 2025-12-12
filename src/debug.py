8ad1133379806f16c40c7bec50cb805d9df7eabf
Klaudia Kyc <klaudia.kyc@gmail.com>
2025-12-12 11:36:04 +0100

    import fix

--- src/debug.py
+++ src/debug.py
@@ -1,6 +1,5 @@
 import pygame
 
-font = pygame.font.SysFont("comicsansms", 9)
 
 def draw_rect(
     screen: pygame.Surface,
@@ -11,20 +10,21 @@ def draw_rect(
     border_width: int = 2,
     show_anchors: bool = True,
 ):
-        r, g, b = color
+    font = pygame.font.SysFont("comicsansms", 9)
+    r, g, b = color
 
-        overlay = pygame.Surface((rect.width, rect.height), pygame.SRCALPHA)
-        overlay.fill((r, g, b, max(0, min(alpha, 255))))
-        screen.blit(overlay, rect.topleft)
-        # obrys
-        pygame.draw.rect(screen, color, rect, width=border_width)
-        # etykieta
-        label = font.render(label, True, color)
-        screen.blit(label, (rect.x + 4, rect.y + 4))
+    overlay = pygame.Surface((rect.width, rect.height), pygame.SRCALPHA)
+    overlay.fill((r, g, b, max(0, min(alpha, 255))))
+    screen.blit(overlay, rect.topleft)
+    # obrys
+    pygame.draw.rect(screen, color, rect, width=border_width)
+    # etykieta
+    label = font.render(label, True, color)
+    screen.blit(label, (rect.x + 4, rect.y + 4))
 
-        if show_anchors:
-            pygame.draw.circle(screen, color, rect.center, 2)  # środek
-            pygame.draw.circle(screen, color, rect.midbottom, 3)  # „stopy”
+    if show_anchors:
+        pygame.draw.circle(screen, color, rect.center, 2)  # środek
+        pygame.draw.circle(screen, color, rect.midbottom, 3)  # „stopy”
 
 def draw_area(
     screen: pygame.Surface,
