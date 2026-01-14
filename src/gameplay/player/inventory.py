70dbe79cd6cca2f8b1d6583606a24eb811fc5b1d
Klaudia Kyc <klaudia.kyc@gmail.com>
2026-01-14 19:01:15 +0100

    refactoring

--- src/gameplay/player/inventory.py
+++ src/gameplay/player/inventory.py
@@ -38,40 +38,50 @@ class InventoryUI:
         self.font = font
         self.item_icons = item_icons
         self._panel = panel
-        self.padding = padding
-        self.line_space = line_space
+        self._padding = padding
+        self._line_space = line_space
+        self._margin_x = ss.game_units_to_px(10)
+        self._margin_y = ss.game_units_to_px(20)
+        self._start_x = self._panel.left + self._margin_x
+        self._start_y = self._panel.top + self._margin_y
+        self._box_width = ss.game_units_to_px(75)
+        self._box_height = ss.game_units_to_px(75)
+        self._gap = ss.game_units_to_px(5)
+        self.dark = (65, 85, 60)
+        self.light = (140, 165, 135)
+        self.slot_columns = 3
+        self.slot_rows = 6
+
 
     def _create_slots(self, screen, inventory):
-        margin_x = ss.game_units_to_px(10)
-        margin_y = ss.game_units_to_px(20)
-        start_x = self._panel.left + margin_x
-        start_y = self._panel.top + margin_y
-        box_width = ss.game_units_to_px(75)
-        box_height = ss.game_units_to_px(75)
-        gap = ss.game_units_to_px(5)
-        dark = (65, 85, 60)
-        light = (140, 165, 135)
         items_list = list(inventory.all_items())
         slot_index = 0
-        for i in range(6):
-            y = start_y + i * (box_height + gap)
-            for j in range(3):
-                x = start_x + j * (box_width + gap)
-                slot = pygame.Rect(x, y, box_width, box_height)
-                pygame.draw.rect(screen, dark, slot, width=5, border_radius=10)
-                pygame.draw.rect(screen, light, slot, width=3, border_radius=10)
-                pygame.draw.rect(screen, dark, slot, width=1, border_radius=10)
-
+        for i in range(self.slot_rows):
+            y = self._start_y + i * (self._box_height + self._gap)
+            for j in range(self.slot_columns):
+                x = self._start_x + j * (self._box_width + self._gap)
+                slot = pygame.Rect(x, y, self._box_width, self._box_height)
+                self._draw_slot(screen, slot)
                 if slot_index < len(items_list):
                     item_id, count = items_list[slot_index]
-                    icon = self.item_icons.get(item_id)
-                    if icon:
-                        icon_rect = icon.get_rect(center=slot.center)
-                        screen.blit(icon, icon_rect)
-                        text = self.font.render(str(count), True, (255, 255, 255))
-                        text_rect = text.get_rect(bottomright=(slot.right - 4*self.padding, slot.bottom - 2*self.line_space))
-                        screen.blit(text, text_rect)
+                    self._draw_picked_item(screen, slot, item_id, count)
                 slot_index += 1
+
+
+    def _draw_slot(self, screen, slot):
+        pygame.draw.rect(screen, self.dark, slot, width=5, border_radius=10)
+        pygame.draw.rect(screen, self.light, slot, width=3, border_radius=10)
+        pygame.draw.rect(screen, self.dark, slot, width=1, border_radius=10)
+
+    def _draw_picked_item(self, screen, slot, item_id, count):
+        icon = self.item_icons.get(item_id)
+        if icon:
+            icon_rect = icon.get_rect(center=slot.center)
+            screen.blit(icon, icon_rect)
+            text = self.font.render(str(count), True, (255, 255, 255))
+            text_rect = text.get_rect(bottomright=(slot.right - 4*self._padding, slot.bottom - 2*self._line_space))
+            screen.blit(text, text_rect)
+        
 
     def draw(self, screen: pygame.Surface, inventory: Inventory):
         self._create_slots(screen, inventory)
